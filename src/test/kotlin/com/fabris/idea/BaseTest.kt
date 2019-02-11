3a702136cc8651264405c7e3c0dff2cb6b63049a
Andrea Fabris <andrea@supapass.com>
2019-02-11 15:32:53 +0000

    Make BaseTest concrete and open
    Update .gitignore
    Added Idea file

--- src/test/kotlin/com/fabris/idea/BaseTest.kt
+++ src/test/kotlin/com/fabris/idea/BaseTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-abstract class BaseTest {
+open class BaseTest {
 
     @Test
     fun testMultiply() {
